8844272f8f65676e66e9672c2b19e7c5c703ab1a
Nathaniel Glewwe <nateglewwe@gmail.com>
2023-11-15 18:35:56 -0600

    almost finished all the questionsjust need to finish the last one

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -11,51 +11,70 @@ function hello() {
 // Call the function to test
 console.log('Test - should say "Hello World!"', hello());
 
-
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName(name) {
-  return;
+  let greeting = `Hello, ${name}!`;
+  return greeting;
 }
 // Remember to call the function to test
-
+console.log(helloName('Nathaniel'));
 
 // 3. Function to add two numbers together & return the result
-function addNumbers(firstNumber) {
+function addNumbers(firstNumber, secondNumber) {
+  let sum = firstNumber + secondNumber;
+  return sum;
+  
   // return firstNumber + secondNumber;
 }
+console.log(addNumbers(8,9));
 
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree() {
-
+function multiplyThree(num1, num2, num3) {
+let product = num1 * num2 * num3;
+return product;
 }
-
+console.log(multiplyThree(2, 3, 4));
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive(number) {
   if (number > 0) {
-    return;
+    return true;
   }
-  return;
+  else return false;
 }
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
-
+console.log(isPositive(1));
+console.log(isPositive(-2));
 
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
 function getLast(array) {
-
+let lastItem = array[array.length - 1];
+return lastItem;
 }
+console.log(getLast([1,2,3,4,5]));
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 function find(value, array) {
-
+let result = false;
+  for (let index = 0; index < array.length; index++) {
+    if (array[index] === value) {
+    result = true;
+    }
+   
+  }
+  return result;
 }
+console.log(find(3, [1,2,3,4,5]));
+
+
+
 
 // ----------------------
 // Stretch Goals
